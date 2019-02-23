b95becc6359f2826c7c15e684af5fbfb887425c9
felipe-sbatista <felipe-sbatista@outlook.com>
2019-02-22 22:27:03 -0300

    develop commit

--- app/src/main/java/com/palavras/unicap/palavrinhas/Activity/MainActivity.java
+++ app/src/main/java/com/palavras/unicap/palavrinhas/Activity/MainActivity.java
@@ -6,10 +6,28 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
+<<<<<<< Updated upstream
+=======
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.palavras.unicap.palavrinhas.Entity.Usuario;
+import com.palavras.unicap.palavrinhas.Persistence.AppDatabase;
+import com.palavras.unicap.palavrinhas.Persistence.DatabaseCopier;
+>>>>>>> Stashed changes
 import com.palavras.unicap.palavrinhas.R;
 import com.palavras.unicap.palavrinhas.Viewmodel.MainViewModel;
 
+<<<<<<< Updated upstream
 import androidx.appcompat.app.AppCompatActivity;
+=======
+import java.util.HashMap;
+import java.util.Map;
+
+>>>>>>> Stashed changes
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -27,11 +45,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+<<<<<<< Updated upstream
         viewModel = new MainViewModel(getApplication());
         if(isFirstTime()){
             viewModel.copyDatabase();
         }
 
+=======
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> u = new HashMap<>();
+        u.put("nome", "felipe");
+        u.put("idade", 5);
+        db.collection("usuarios").add(u).addOnSuccessListener(documentReference -> System.out.println(documentReference.getId()));
+>>>>>>> Stashed changes
     }
 
 
